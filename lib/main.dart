9b72636e5a1b1714eb58b2c9e7c288af37b7692c
Sushil <baymax732001@gmail.com>
2023-01-11 12:10:00 +0530

    Go Router

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:auth_riverpod_gorouter/providers/router_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:get/get_navigation/src/root/get_material_app.dart';
 
 void main() {
   runApp(const ProviderScope(child: MyApp()));
@@ -13,7 +14,7 @@ class MyApp extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final router = ref.watch(routerProvider);
-    return MaterialApp.router(
+    return GetMaterialApp.router(
       title: 'Auth Riverpod GoRouter',
       theme: ThemeData(
         primarySwatch: Colors.deepOrange,
